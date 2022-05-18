1bae169b72c04c27d570211465b1096c65d59e93
Diar <diarselimi92@gmail.com>
2022-05-19 00:52:48 +0200

    init

---
+++ www/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo app test</title>
+</head>
+<body>
+  <div class="wrapper">
+    <header>Aplikacioi, Data Test: <?php echo date('Y-m-d'); ?></header>
+    <div class="inputField">
+      <input type="text" placeholder="Shto nje TODO">
+      <button>Shto</button>
+    </div>
+    <ul class="todoList">
+    </ul>
+  </div>   
+</body>
+
+</html>
